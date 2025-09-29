c0451868b8b65c459f4b7e62c17d6e59e9a865c1
PedroRockembach <rockpedro11@gmail.com>
2025-09-29 13:17:16 -0300

    adicionando funcoes
    está sendo construido varias funções para ajudar em exercicios de c++

---
+++ funções/uteis.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+string to_lower(string s) 
+{
+	for (char &i : s) 
+	{
+		if (i >= 'A' && i <= 'Z')
+		{
+			i = i + 32;
+		}
+	}
+	return s;
+}
+string to_upper(string s)
+{
+	for (char &i : s)
+	{
+		if (i >= 'a' && i <= 'z')
+		{
+			i = i - 32;
+		}
+	}
+	return s;
+}
+int main()
+{
+	string lower = to_lower("Casei Na Rua Sete");
+	cout << lower;
+	
+	return 0;
+}
